939ebf500c2d3fffd49308faf0972bfd8c0cda5d
gowthnar <gowthnar@GOWTHNAR-IN.in.oracle.com>
2020-10-04 17:44:35 +0530

    jar to war conversion

--- src/main/java/com/cars24/auction/AuctionApplication.java
+++ src/main/java/com/cars24/auction/AuctionApplication.java
@@ -2,12 +2,19 @@ package com.cars24.auction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AuctionApplication {
+public class AuctionApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AuctionApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuctionApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionApplication.class, args);
+    }
 
 }
