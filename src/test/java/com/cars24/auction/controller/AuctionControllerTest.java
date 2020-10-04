7fdf75e1706492a4350e69f9ce49e0f33b223d52
gowthnar <gowthnar@GOWTHNAR-IN.in.oracle.com>
2020-10-04 21:11:27 +0530

    Remove unwanted dependency

--- src/test/java/com/cars24/auction/controller/AuctionControllerTest.java
+++ src/test/java/com/cars24/auction/controller/AuctionControllerTest.java
@@ -29,59 +29,60 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = AuctionController.class)
 @WithMockUser
-public class AuctionControllerTest  {
+public class AuctionControllerTest {
     @Autowired
-	private MockMvc mockMvc;
+    private MockMvc mockMvc;
     @MockBean
-	private AuctionService auctionService;
+    private AuctionService auctionService;
 
     String exampleBidJson = "{\"bidAmount\":\"1200\"}";
 
     @Test
     public void getRunningAuctions() throws Exception {
-        RunningAuctionDTO run=new RunningAuctionDTO("BMW7",1200,200);
-        List<RunningAuction> runningAuctions=Arrays.asList(run);
+        RunningAuctionDTO run = new RunningAuctionDTO("BMW7", 1200, 200);
+        List<RunningAuction> runningAuctions = Arrays.asList(run);
         Mockito.when(
-				auctionService.fetchRunningAuctions(Mockito.anyString(),
-						Mockito.anyString(),Mockito.anyString())).thenReturn(runningAuctions);
+                auctionService.fetchRunningAuctions(Mockito.anyString(),
+                        Mockito.anyString(), Mockito.anyString())).thenReturn(runningAuctions);
 
         String uri = "/auction?status=RUNNING";
         RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
-				uri).accept(
-				MediaType.APPLICATION_JSON);
-         mockMvc.perform(requestBuilder).andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(1)));
+                uri).accept(
+                MediaType.APPLICATION_JSON);
+        mockMvc.perform(requestBuilder).andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(1)));
     }
+
     @Test
-    public void postBid () throws Exception{
+    public void postBid() throws Exception {
         //Accepted
         Mockito.when(
-				auctionService.postBid(Mockito.anyString(),
-						Mockito.anyInt())).thenReturn(ApplicationConstants.ACCEPTED);
+                auctionService.postBid(Mockito.anyString(),
+                        Mockito.anyInt())).thenReturn(ApplicationConstants.ACCEPTED);
         String uri = "/auction/BMW8/bid";
         RequestBuilder requestBuilder = MockMvcRequestBuilders.post(
-				uri).accept(
-				MediaType.APPLICATION_JSON).content(exampleBidJson).contentType(MediaType.APPLICATION_JSON);
+                uri).accept(
+                MediaType.APPLICATION_JSON).content(exampleBidJson).contentType(MediaType.APPLICATION_JSON);
         mockMvc.perform(requestBuilder).andExpect(status().isCreated());
         //Rejected
         exampleBidJson = "{\"bidAmount\":\"1100\"}";
-         Mockito.when(
-				auctionService.postBid(Mockito.anyString(),
-						Mockito.anyInt())).thenReturn(ApplicationConstants.NOT_ACCEPTED);
+        Mockito.when(
+                auctionService.postBid(Mockito.anyString(),
+                        Mockito.anyInt())).thenReturn(ApplicationConstants.NOT_ACCEPTED);
 
         requestBuilder = MockMvcRequestBuilders.post(
-				uri).accept(
-				MediaType.APPLICATION_JSON).content(exampleBidJson).contentType(MediaType.APPLICATION_JSON);
+                uri).accept(
+                MediaType.APPLICATION_JSON).content(exampleBidJson).contentType(MediaType.APPLICATION_JSON);
         mockMvc.perform(requestBuilder).andExpect(status().isNotAcceptable());
         //Action(Item) not found
         exampleBidJson = "{\"bidAmount\":\"1300\"}";
         uri = "/auction/YYY/bid";
         Mockito.when(
-				auctionService.postBid(Mockito.anyString(),
-						Mockito.anyInt())).thenReturn(ApplicationConstants.NOT_FOUND);
+                auctionService.postBid(Mockito.anyString(),
+                        Mockito.anyInt())).thenReturn(ApplicationConstants.NOT_FOUND);
 
         requestBuilder = MockMvcRequestBuilders.post(
-				uri).accept(
-				MediaType.APPLICATION_JSON).content(exampleBidJson).contentType(MediaType.APPLICATION_JSON);
+                uri).accept(
+                MediaType.APPLICATION_JSON).content(exampleBidJson).contentType(MediaType.APPLICATION_JSON);
         mockMvc.perform(requestBuilder).andExpect(status().isNotFound());
     }
 }
